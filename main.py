3f1a439c11376ff1f77d97272579fed28c1615b6
Maoyurun Mao <mao3012633778@gmail.com>
2025-06-25 12:46:58 +0200

    acq_func

--- main.py
+++ main.py
@@ -1,21 +1,22 @@
 import torch
 from models import gp_model
+from optimization import qLogEHVI
 
 # 假设 train_X 形状为 (N, 4) -> 4个工艺参数
 train_x = torch.tensor([
     [100, 800, 0.1, 0.08],
     [120, 1000, 0.1, 0.10],
-    [80,  900, 0.12, 0.07],
+    [80, 900, 0.12, 0.07],
     [110, 1100, 0.1, 0.09],
-    [95,  850, 0.11, 0.08],
+    [95, 850, 0.11, 0.08],
     [105, 950, 0.1, 0.07],
-    [90,  1000, 0.13, 0.1],
+    [90, 1000, 0.13, 0.1],
     [115, 970, 0.09, 0.08],
 ], dtype=torch.double)
 
 # train_Y shape = (N, 3) -> 三个目标
 train_y = torch.tensor([
-    [0.98, 8.5, 12.0],   # [Density, Roughness, Time]
+    [0.98, 8.5, 12.0],  # [Density, Roughness, Time]
     [0.96, 9.0, 10.5],
     [0.92, 10.5, 11.5],
     [0.97, 8.0, 10.0],
@@ -26,15 +27,14 @@ train_y = torch.tensor([
 ], dtype=torch.double)
 
 model = gp_model.build_model(train_x, train_y)
-# TEST
-test_x = torch.tensor([
-    [100, 950, 0.1, 0.08],
-    [110, 1000, 0.1, 0.09],
-    [90, 900, 0.12, 0.07],
-    [100, 800, 0.1, 0.08],
-], dtype=torch.double)
 
-test_y = gp_model.predict(model, test_x)
+ref_point = [0.9, 11.0, 13.0]  # [density (min), roughness (max), time (max)]
 
-print(test_y)
+bounds = [
+    [80, 800, 0.09, 0.07],
+    [120, 1100, 0.13, 0.10]
+]
 
+sample, a = qLogEHVI.optimize_acq_fun(model, train_y, bounds, batch_size=3, ref_point=ref_point)
+print(sample)
+print(a)
---
+++ optimization/qLogEHVI.py
@@ -0,0 +1,92 @@
+from botorch.acquisition.multi_objective import IdentityMCMultiOutputObjective, qLogExpectedHypervolumeImprovement
+from botorch.optim import optimize_acqf
+from botorch.sampling.normal import SobolQMCNormalSampler
+from botorch.utils.multi_objective.box_decompositions import NondominatedPartitioning
+import torch
+
+
+def optimize_acq_fun(model, train_y, bounds, batch_size=3, ref_point=None, slack=None):
+    """
+    Build a qLogExpectedHypervolumeImprovement acquisition function for multi-objective BO.
+
+    :param model: A fitted ModelListGP model (multi-objective surrogate).
+    :param train_y: Current training targets (shape: N x M).
+    :param bounds: Limit of the tasks
+    :param batch_size: The size of the returned suggestion sample (default: 3).
+    :param ref_point: Reference point for hyper-volume calculation (Shape: M, Tensor or list).
+    :param slack: Slack to get ref_point automatically (Shape: M)
+    :return: candidate (q x d), acquisition values
+    """
+    if ref_point is None and slack is None:
+        raise ValueError("You must provide either a ref_point or a slack value to compute.")
+
+    if ref_point is None:
+        ref_point = get_ref_point(train_y, slack)
+
+    if not torch.is_tensor(bounds):
+        bounds = torch.tensor(bounds, dtype=torch.double)
+    if not torch.is_tensor(ref_point):
+        ref_point = torch.tensor(ref_point, dtype=torch.double)
+
+    acq_func = build_acq_fun(model, ref_point, train_y)
+
+    candidate, acq_value = optimize_acqf(
+        acq_function=acq_func,
+        bounds=bounds,
+        q=batch_size,
+        num_restarts=10,  # Repeat optimization times with different starting points (to prevent local optimum)
+        raw_samples=100,  # Initial random sample number (used to find initial value)
+        return_best_only=True,  # Only return optimal solution
+    )
+    return candidate, acq_value  # suggested samples and average acq_value
+
+
+def get_ref_point(train_y, slack):
+    """
+        Find a reference point for hyper-volume optimization.
+
+        :param train_y: Current training targets (shape: N x M).
+        :param slack: Slack to get ref_point automatically (Shape: M)
+        :return: A reference point (shape: M).
+        """
+    ref_point = []
+    for i in range(train_y.shape[1]):
+        ref = train_y[:, i].max().item() + (slack[i] if isinstance(slack, list) else slack)
+        ref_point.append(ref)
+    return ref_point
+
+
+def build_acq_fun(model, ref_point, train_y):
+    """
+    Build a qLogExpectedHypervolumeImprovement acquisition function for multi-objective BO.
+
+    :param model: A fitted ModelListGP model (multi-objective surrogate).
+    :param ref_point: Reference point for hyper-volume calculation (Tensor or list).
+    :param train_y: Current training targets (shape: N x M).
+    :return: A qLogEHVI acquisition function object.
+    """
+    if not torch.is_tensor(ref_point):
+        ref_point = torch.tensor(ref_point, dtype=torch.double)
+
+    sampler = SobolQMCNormalSampler(sample_shape=torch.Size([128]), seed=42)
+
+    objective = IdentityMCMultiOutputObjective()
+
+    y_pareto = train_y
+
+    partitioning = NondominatedPartitioning(
+        ref_point=ref_point,
+        Y=y_pareto
+    )
+
+    acq_func = qLogExpectedHypervolumeImprovement(
+        model=model,
+        ref_point=ref_point,
+        partitioning=partitioning,
+        sampler=sampler,
+        objective=objective
+    )
+    return acq_func
+
+
+
